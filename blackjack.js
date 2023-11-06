886bc47f5577d4f276942c8c56b1a330b2e5f91c
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-06 11:57:16 -0800

    Attempt document.write() and fix format

--- blackjack.js
+++ blackjack.js
@@ -15,7 +15,7 @@ class CardPlayer {
 		let randomCard = blackjackDeck[Math.floor(Math.random() * blackjackDeck.length)];
 		this.hand.push(randomCard);
 	}
-};
+}
 
 // // CREATE TWO NEW CardPlayers
 const dealer = new CardPlayer('Dealer');
@@ -38,7 +38,7 @@ const calcPoints = (hand) => {
 	let total = 0;
 	hand.forEach((hand) => {
 		total += hand.val;
-	});
+	})
 
 	// Count number of Aces in hand
 	let aceExist = hand.filter((card) => card.displayVal === 'Ace');
@@ -47,8 +47,8 @@ const calcPoints = (hand) => {
 	// For more than one Ace,
 	if (numAces > 0) {
 
-		// Calculate number of mandatory deductions 
-		// Maximum of 1 Ace value at 11 
+		// Calculate number of mandatory deductions from available Aces
+		// with maximum of 1 Ace valued at 11 
 		deductAllButOne = (numAces - 1) * 10;
 		total -= deductAllButOne;
 
@@ -57,8 +57,8 @@ const calcPoints = (hand) => {
 			// total = total;
 		}
 		if (total > 21) {
-			// Apply optional deduction from remaining Ace
-			// isSoft = false;
+			// Apply optional deduction from last remaining Ace
+			// isSoft = false; // All aces are 1
 			total -= 10;
 		}
 	}
@@ -98,13 +98,16 @@ const dealerShouldDraw = (dealerHand) => {
 
 const determineWinner = (playerScore, dealerScore) => {
 	// All points over 21 has been accounted for in startGame() script
-
+	// Calculated points printed to console prior to this
 	if (dealerScore < playerScore) {
-		return `Player has ${playerScore} points. Dealer has ${dealerScore} points. Player wins`;
+		document.write(`Player wins.`);
+		return `Player wins.`;
 	} else if (dealerScore > playerScore) {
-		return `Player has ${playerScore} points. Dealer has ${dealerScore} points. Dealer wins`;
+		document.write(`Dealer wins.`);
+		return `Dealer wins.`;
 	} else {
-		return `Player has ${playerScore} points. Dealer has ${dealerScore} points. It's a tie`;
+		document.write(`It's a tie.`);
+		return `It's a tie.`;
 	}
 };
 
@@ -114,7 +117,7 @@ const determineWinner = (playerScore, dealerScore) => {
  * @param {string} dealerCard 
  */
 const getMessage = (count, dealerCard) => {
-	return `Dealer showing ${dealerCard.displayVal} of value ${dealerCard.val}, your count is ${count}.  Draw card?`;
+	return `Dealer showing ${dealerCard.displayVal} of value ${dealerCard.val}, your count is ${count}. Draw card?`;
 }
 
 /**
@@ -123,8 +126,9 @@ const getMessage = (count, dealerCard) => {
  */
 let showHand = (player) => {
 	let displayHand = player.hand.map((card) => card.displayVal);
-	// Added to input onto HTML (does not automatically write due to prompt message)
+	// Input onto HTML (does not automatically write due to prompt)
 	document.write(`${player.name}'s hand is ${displayHand.join(', ')} (${calcPoints(player.hand).total}). <br>`);
+	// Write to console
 	console.log(`${player.name}'s hand is ${displayHand.join(', ')} (${calcPoints(player.hand).total})`);
 }
 
@@ -138,45 +142,57 @@ const startGame = function () {
 	player.drawCard();
 	dealer.drawCard();
 
+	// Calculate Player's score 
 	let playerScore = calcPoints(player.hand).total;
 	showHand(player);
 
+	// Calculate Dealer's score 
 	let dealerScore = calcPoints(dealer.hand).total;
 
-	// Show of player's and dealer's hand
+	// Show player's and dealer's hand of first 2 cards
 	let playerFirstHand = player.hand.map((card) => card.displayVal);
 	let dealerFirstHand = dealer.hand.map((card) => card.displayVal);
 
-	// End game if Player or Dealer has 21
+	// Write dealer's first hand into HTML before moving on
+	document.write(`${dealer.name}'s hand is ${dealerFirstHand.join(', ')} (${dealerScore}). <br>`);
+
+	// End game if Player or Dealer has 21 
 	if (playerScore === 21 && dealerScore != 21) {
 		document.write(`Player wins ! <br>`);
-		return `Player wins !`; 								// showhand() prints total and displayVal before this
+		// showhand() prints total and displayVal before this
+		return `Player wins !`;
 	} else if (dealerScore === 21 && playerScore != 21) {
 		document.write(`Dealer wins!`);
-		return ` Dealer wins!`;
+		return `Dealer's hand is ${dealerFirstHand.join(', ')} (21). Dealer wins!`;
 	} else if (dealerScore === 21 && playerScore === 21) {
 		document.write(`Player's hand is ${playerFirstHand.join(', ')} (21) and Dealer's hand is ${dealerFirstHand.join(', ')} (21). It's a tie`);
 		return `Player's hand is ${playerFirstHand.join(', ')} (21) and Dealer's hand is ${dealerFirstHand.join(', ')} (21). It's a tie`;
 	}
 
+	// Player's turn to draws if less than 21
 	while (playerScore < 21 && confirm(getMessage(playerScore, dealer.hand[0]))) {
 		player.drawCard();
 		playerScore = calcPoints(player.hand).total;
 		showHand(player);
 	}
 	if (playerScore > 21) {
-		return 'You went over 21 - you lose!';
+		document.write(`You went over 21 - you lose! <br>`);
+		return `You went over 21 - you lose!`;
 	}
-	console.log(`Player stands at ${playerScore}`);
+	document.write(`Player stands at ${playerScore}. <br>`);
+	console.log(`Player stands at ${playerScore}.`);
 
+	// Dealer's turn to draw 
 	while (dealerScore < 21 && dealerShouldDraw(dealer.hand)) {
 		dealer.drawCard();
 		dealerScore = calcPoints(dealer.hand).total;
 		showHand(dealer);
 	}
 	if (dealerScore > 21) {
-		return 'Dealer went over 21 - you win!';
+		document.write(`Dealer went over 21 - you win! <br>`);
+		return `Dealer went over 21 - you win!`;
 	}
+	document.write(`Dealer stands at ${dealerScore} <br>`);
 	console.log(`Dealer stands at ${dealerScore}`);
 
 	return determineWinner(playerScore, dealerScore);
